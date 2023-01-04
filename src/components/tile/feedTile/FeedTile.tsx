f4697c6045524ae3ca13ace90a9ff2c60f05611b
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2023-01-04 18:44:19 +0100

    feat: move views to separate dirs

--- src/components/tile/feedTile/FeedTile.tsx
+++ src/components/tile/feedTile/FeedTile.tsx
@@ -2,13 +2,14 @@ import { memo } from "react";
 
 interface FeedTileProps {
   readonly url: string;
-  readonly onDelete: ({ url }: { url: string }) => void;
+  readonly onDelete: ({ url }: { url: string }) => Promise<void>;
 }
 
 export const FeedTile = memo<FeedTileProps>(({ url, onDelete }) => {
   return (
     <div>
       {url}
+      {/* eslint-disable-next-line @typescript-eslint/no-misused-promises*/}
       <button onClick={() => onDelete({ url })}>Delete feed</button>
     </div>
   );
---
+++ src/pages/dashboard/settings.tsx
@@ -0,0 +1,9 @@
+import { SettingsView } from "src/views/dashboard/settings/Settings";
+
+import type { NextPage } from "next";
+
+const Settings: NextPage = () => {
+  return <SettingsView />;
+};
+
+export default Settings;
