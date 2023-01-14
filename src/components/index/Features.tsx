efa07d74d5de224a6799b0dc2a1f1070b541cc2b
Krzysztof <halidu13@gmail.com>
2023-01-14 21:36:46 +0100

    feat: add Features section

---
+++ src/components/index/Features.tsx
@@ -0,0 +1,42 @@
+import { Card } from "./Card";
+import { SectionHeading } from "./SectionHeading";
+import { SectionLayout } from "./SectionLayout";
+
+const FEATURES_CARDS = [
+  {
+    title: "Seamless Syncing",
+    description:
+      "Automatically sync your favorite RSS feeds to your Remarkable tablet",
+    className:
+      "bg-gradient bg-gradient-to-bl from-indigo-400/10 to-indigo-500/30",
+  },
+  {
+    title: "Add any RSS feed",
+    description:
+      "Any RSS feed can be synced to your Remarkable tablet. No restrictions",
+    className:
+      "bg-gradient bg-gradient-to-bl from-indigo-400/5 to-indigo-500/20",
+  },
+];
+
+export const Features = () => {
+  return (
+    <SectionLayout>
+      <SectionHeading
+        title="Features"
+        description="RSSMarkable is a free service that allows you to sync your Remarkable feeds"
+      />
+
+      <div className="flex flex-col items-stretch justify-center gap-4 md:flex-row">
+        {FEATURES_CARDS.map((card) => (
+          <Card
+            key={card.title}
+            title={card.title}
+            description={card.description}
+            className={card.className}
+          />
+        ))}
+      </div>
+    </SectionLayout>
+  );
+};
