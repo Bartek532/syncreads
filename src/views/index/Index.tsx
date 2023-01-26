fb8dd4778e65e50696366a9fac5bdd05ab6bf850
Krzysztof <halidu13@gmail.com>
2023-01-26 22:18:51 +0100

    fix: remove memo from index view

--- src/views/index/Index.tsx
+++ src/views/index/Index.tsx
@@ -1,5 +1,3 @@
-import { memo } from "react";
-
 import { Features } from "../../components/index/Features";
 import { Footer } from "../../components/index/Footer";
 import { GetStartedSteps } from "../../components/index/GetStartedSteps";
@@ -8,7 +6,7 @@ import { Layout } from "../../components/index/Layout";
 import { Nav } from "../../components/index/Nav";
 import { OpenSource } from "../../components/index/OpenSource";
 
-export const IndexView = memo(() => {
+export const IndexView = () => {
   return (
     <Layout>
       <Nav />
@@ -19,6 +17,6 @@ export const IndexView = memo(() => {
       <Footer />
     </Layout>
   );
-});
+};
 
 IndexView.displayName = "IndexView";
