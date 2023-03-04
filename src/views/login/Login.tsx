e2cc052309ed9ffc96df247172f87d9605f130bd
Krzysztof <halidu13@gmail.com>
2023-03-04 20:54:51 +0100

    fix: imports sort

--- src/views/login/Login.tsx
+++ src/views/login/Login.tsx
@@ -1,9 +1,9 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { signIn } from "next-auth/react";
 import dynamic from "next/dynamic";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { signIn } from "next-auth/react";
 import { memo, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
