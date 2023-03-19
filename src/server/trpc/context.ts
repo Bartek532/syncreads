8e0956476c6b0e6d3b4abd4364258d46064bd030
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2023-03-19 14:56:08 +0100

    feat: configure websocket server

--- src/server/trpc/context.ts
+++ src/server/trpc/context.ts
@@ -1,13 +1,21 @@
-import { getServerAuthSession } from "../common/get-server-auth-session";
+import EventEmitter from "events";
+import { getSession } from "next-auth/react";
+
 import { prisma } from "../db/client";
 
 import type { inferAsyncReturnType } from "@trpc/server";
 import type { CreateNextContextOptions } from "@trpc/server/adapters/next";
+import type { NodeHTTPCreateContextFnOptions } from "@trpc/server/dist/adapters/node-http";
+import type { IncomingMessage } from "http";
 import type { Session } from "next-auth";
+import type ws from "ws";
 
 interface CreateContextOptions {
   session: Session | null;
+  ee: EventEmitter;
 }
+
+const ee = new EventEmitter();
 
 /** Use this helper for:
  * - testing, so we dont have to mock Next.js' req/res
@@ -17,6 +25,7 @@ interface CreateContextOptions {
 export const createContextInner = (opts: CreateContextOptions) => {
   return {
     session: opts.session,
+    ee: opts.ee,
     prisma,
   };
 };
@@ -25,14 +34,19 @@ export const createContextInner = (opts: CreateContextOptions) => {
  * This is the actual context you'll use in your router
  * @link https://trpc.io/docs/context
  **/
-export const createContext = async (opts: CreateNextContextOptions) => {
-  const { req, res } = opts;
+export const createContext = async (
+  opts:
+    | CreateNextContextOptions
+    | NodeHTTPCreateContextFnOptions<IncomingMessage, ws>,
+) => {
+  const session = await getSession(opts);
 
   // Get the session from the server using the unstable_getServerSession wrapper function
-  const session = await getServerAuthSession({ req, res });
+  //const session = await getServerAuthSession({ req, res });
 
   return createContextInner({
     session,
+    ee,
   });
 };
 
