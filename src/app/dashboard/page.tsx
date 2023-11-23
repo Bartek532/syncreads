744d8f0d8789dc27f4b9228a3d2b74493bcd35fb
Filip Krzemień <filipos2003@gmail.com>
2023-11-23 22:18:57 +0100

    refactor: added dashborad  page to app router and delated it from pages directory

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,10 @@
+"use client";
+
 import { Seo } from "../../components/common/Seo";
 import { DashboardLayout } from "../../components/dashboard/layout/Layout";
 import { HomeView } from "../../views/dashboard/Home";
 
-import type { NextPage } from "next";
-
-const Dashboard: NextPage = () => {
+const Dashboard = () => {
   return (
     <>
       <Seo title="Dashboard - Home" />
