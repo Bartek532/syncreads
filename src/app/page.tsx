af12eda5b622033f931b14dd95e40326194ee4f4
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2023-11-23 18:45:42 +0100

    refactor: migrate index page to RSC

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,7 @@
-import { Seo } from "../components/common/Seo";
-import { api } from "../trpc/server";
 import { IndexView } from "../views/index/Index";
 
-const Home = async () => {
-  const xd = await api.feed.getAllFeeds.query();
-
-  return (
-    <>
-      {JSON.stringify(xd)}
-      <h1>Komponent</h1>
-    </>
-  );
+const Home = () => {
+  return <IndexView />;
 };
 
 export default Home;
