f44db7f69937cb9045397eb47c11b9dc2f4eb9cd
Filip Krzemień <filipos2003@gmail.com>
2023-11-25 02:53:42 +0100

    refactor: added dashboard page to app directory

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,18 @@
+import { Seo } from "../../components/common/Seo";
+import { DashboardLayout } from "../../components/dashboard/layout/Layout";
+import { HomeView } from "../../views/dashboard/Home";
+
+import type { NextPage } from "next";
+
+const Dashboard: NextPage = () => {
+  return (
+    <>
+      <Seo title="Dashboard - Home" />
+      <DashboardLayout>
+        <HomeView />
+      </DashboardLayout>
+    </>
+  );
+};
+
+export default Dashboard;
