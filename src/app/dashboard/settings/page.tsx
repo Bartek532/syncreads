8829549a7953d87532ab8f55b3701bc55aaa6e4a
Filip Krzemień <filipos2003@gmail.com>
2023-11-25 03:08:14 +0100

    refactor: added settings page to app directory

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -1,6 +1,6 @@
-import { Seo } from "../../components/common/Seo";
-import { DashboardLayout } from "../../components/dashboard/layout/Layout";
-import { SettingsView } from "../../views/dashboard/settings/Settings";
+import { Seo } from "../../../components/common/Seo";
+import { DashboardLayout } from "../../../components/dashboard/layout/Layout";
+import { SettingsView } from "../../../views/dashboard/settings/Settings";
 
 import type { NextPage } from "next";
 
--- src/views/dashboard/settings/Settings.tsx
+++ src/views/dashboard/settings/Settings.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Switch } from "@headlessui/react";
 import { twMerge } from "tailwind-merge";
 
