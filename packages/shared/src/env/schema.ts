9481d2dfb427dac4d7a1acb514f8b3a9d66b0fc0
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2023-12-22 21:04:03 +0100

    chore: update .env.example

--- packages/shared/src/env/schema.ts
+++ packages/shared/src/env/schema.ts
@@ -6,7 +6,14 @@ const sharedSchema = z.object({
   NODE_ENV: nodeEnvs.default(NODE_ENV.DEVELOPMENT).optional(),
 });
 
-export const anonDatabaseSchema = sharedSchema.merge(
+const authDatabaseSchema = z.object({
+  GITHUB_CLIENT_ID: z.string(),
+  GITHUB_CLIENT_SECRET: z.string(),
+  GOOGLE_CLIENT_ID: z.string(),
+  GOOGLE_CLIENT_SECRET: z.string(),
+});
+
+export const anonDatabaseSchema = sharedSchema.merge(authDatabaseSchema).merge(
   z.object({
     NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
     NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
@@ -22,21 +29,16 @@ export const serviceDatabaseSchema = sharedSchema.merge(
 
 export const serverSchema = sharedSchema.merge(
   z.object({
-    CHROME_BIN: z.string(),
-
     REDIS_HOST: z.string(),
     REDIS_PORT: z.coerce.number(),
     REDIS_PASSWORD: z.string(),
+
+    CHROME_BIN: z.string(),
   }),
 );
 
 export const clientSchema = sharedSchema.merge(
   z.object({
-    GITHUB_CLIENT_ID: z.string(),
-    GITHUB_CLIENT_SECRET: z.string(),
-    GOOGLE_CLIENT_ID: z.string(),
-    GOOGLE_CLIENT_SECRET: z.string(),
-
     SYNC_API_URL: z.string().url(),
   }),
 );
